bd08e6aad6285c79a917ad654bc44ecdbab503a4
oumaima-fitah <oumaima.fitah@etu.uae.ac.ma>
2022-12-09 14:24:13 -0800

    task9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,15 +9,15 @@ int main(void)
 {
 	int n;
 
-	for (n = 48; n < 58; n++)
+	for (n = 0; n < 100; n++)
 	{
-		putchar(n);
-		if (n != 57)
+		putchar(n + '0');
+		if (n < 99)
 		{
 			putchar(',');
 			putchar(' ');
 		}
-	putchar('\n');
 	}
+putchar('\n');
 return (0);
 }
