7131c175e0dae1e9cabdf46fb274a94fff74daf3
oumaima-fitah <oumaima.fitah@etu.uae.ac.ma>
2022-12-09 07:38:58 -0800

    task1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -17,7 +17,7 @@ n = rand() - RAND_MAX / 2;
 a = n % 10;
 if (a > 5)
 {
-printf("Last digital of %d is %d and is greater than 5\n", n, a);
+printf("Last digit of %d is %d and is greater than 5\n", n, a);
 }
 else if (a == 0)
 {
@@ -25,7 +25,7 @@ printf("Last digit of %d is %d and is 0\n", n, a);
 }
 else
 {
-printf("Last digit of %d is %d and is less tahn 6 and not 0\n", n, a);
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, a);
 }
 return (0);
 }
